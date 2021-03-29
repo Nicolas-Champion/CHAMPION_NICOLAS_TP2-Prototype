3d3d8b2c370df520529b187ae5da2608517d3471
Nicolas-Champion <1976202@cegepat.qc.ca>
2021-03-29 18:54:27 -0400

    Ajout de scripts
    ajouté quelques scripts encore incomplets pour l'instant

---
+++ Assets/Scripts/Arme.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arme : MonoBehaviour
+{
+    public Transform barrel;
+    public LineRenderer bulletTrail;
+
+    public float explosionRadius;
+    public float explosionForce;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        //Bullet Trail(pointA)
+        bulletTrail.SetPosition(0, barrel.position);
+
+        //Créer un rayon qui va vers l'avant du pistolet
+        Ray pistolRay = new Ray(barrel.position, barrel.forward);
+        RaycastHit hit;
+
+        //Impact du rayon?
+        if (Physics.Raycast(pistolRay, out hit, 50f))
+        {
+            //bulletTrail (pointB)
+            bulletTrail.SetPosition(1, hit.point);
+
+            //mini-Explosion
+            Explosion explosion = new Explosion(explosionForce, hit.point, explosionRadius, 0.5f);
+
+        }
+
+        else
+        {
+            //Bullet Trail (point B Raté)
+            bulletTrail.SetPosition(1, barrel.position + barrel.forward * 50f);
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.magenta;
+        Gizmos.DrawRay(barrel.position, barrel.forward * 50f);
+    }
+}
---
+++ Assets/Scripts/RbCharacterMovements.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class RbCharacterMovements : MonoBehaviour
+{
+    public float speed = 2f;
+    public float jumpHeight = 1f;
+
+    // Transform de la position des pieds
+    public Transform feetPosition;
+
+    private float inputVertical;
+    private float inputHorizontal;    
+
+    private Vector3 moveDirection;
+
+    private Rigidbody rb;
+
+    private bool isGrounded = true;
+
+    void Start()
+    {
+        // Modifier le layer pour que l'objet puisse uniquement sauté quand il touche le sol (il ne sera plus sur le layer Default)
+        gameObject.layer = 3;
+
+        // Assigner le Rigidbody
+        rb = GetComponent<Rigidbody>();
+
+        // Masse du Rigidbody
+        rb.mass = 8f;
+
+        // Geler la rotation physique
+        rb.freezeRotation = true;
+    }
+
+    void Update()
+    {
+        // Vérifier si l'on touche le sol
+        isGrounded = Physics.CheckSphere(feetPosition.position, 0.15f, 1, QueryTriggerInteraction.Ignore);
+
+        // Vérifier les inputs du joueur
+        // Vertical (W, S et Joystick avant/arrière)
+        inputVertical = Input.GetAxis("Vertical");
+        // Horizontal (A, D et Joystick gauche/droite)
+        inputHorizontal = Input.GetAxis("Horizontal");
+
+        // Vecteur de mouvements (Avant/arrière + Gauche/Droite)
+        moveDirection = transform.forward * inputVertical + transform.right * inputHorizontal;
+
+        // Rotation du personnage
+        Rotate();
+
+        // Sauter
+        if (Input.GetButtonDown("Jump") && isGrounded)        
+            Jump();        
+    }
+
+    void Rotate()
+    {
+        Vector3 rotPlayer = transform.rotation.eulerAngles;
+
+        // Le player tourne en fonction de la position de la souris (y seulement)
+        rotPlayer.y += Input.GetAxis("Mouse X");
+
+        // Appliquer la rotation rotPlayer dans la rotation du Transform (Quaternion)
+        transform.rotation = Quaternion.Euler(rotPlayer);
+    }
+
+    void Jump()
+    {
+        rb.AddForce(Vector3.up * Mathf.Sqrt(jumpHeight * -2f * Physics.gravity.y), ForceMode.VelocityChange);
+    }
+
+    private void FixedUpdate()
+    {
+        // Déplacer le personnage selon le vecteur de direction
+        rb.MovePosition(rb.position + moveDirection * speed * Time.fixedDeltaTime);
+    }
+}
